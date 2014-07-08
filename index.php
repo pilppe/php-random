0242a9d954246be8965955cc8af8ced2542f4462
pilppe <nikkederp@gmail.com>
2014-07-08 14:44:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-//Im creating a login screen, so thats why its under development.
-<?php
-echo "Under development";
-<?
+//I'm not gonna create a database, its up to you.
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>PHP Random</title>
+    <link href="Site.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="main">
+    <?php include("Header.php")
+    <h1>Welcome to Pilppe Github</h1>
+    <h2>will be something</h2>
+    <p>will be something</p>
+    <p>will be something</p>
+    <p>will be something</p>
+    <?php include("Footer.php"); ?>
+</div>
+
+</body>
+</html>
